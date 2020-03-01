31f73630ef6d5ed0fdae6aea3891740293fb6c31
Grzegorz Śnieciński <gsniecinski@gmail.com>
2020-03-01 18:47:42 -0300

    add converting html hex string to Color, add webRequestAwaiter

--- Extensions/ColorExt.cs
+++ Extensions/ColorExt.cs
@@ -14,5 +14,24 @@ namespace Deadbit.Utils.Extensions
         {
             return string.Format("RGB({0}{1}{2})", (int)(c.r * 255), (int)(c.g * 255), (int)(c.b * 255));
         }
+
+        public static Color HexRGBToColor(this string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            return new Color(r / 255f, g / 255f, b / 255f);
+        }
+
+        public static Color HexRGBAToColor(this string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+            return new Color(r / 255f, g / 255f, b / 255f, a / 255f);
+        }
     }
 }
